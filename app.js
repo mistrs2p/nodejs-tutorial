c5ec9438d2f020bf24f107584ac6bd9a42f59fbc
MistRain <mahdimousavi40@gmail.com>
2024-05-29 12:35:48 +0330

    event loop training and ...

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const _ = require('lodash');
+// const _ = require('lodash');
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems)
+// const items = [1, [2, [3, [4]]]];
+// const newItems = _.flattenDeep(items);
+// console.log(newItems)
+
+// console.log("HArahaasd")
+
+
+console.log("First task")
+console.time('MyTimer')
+for (let i= 0; i<100000000; i++) {
+  const h3 = document.querySelector('h3');
+  h3.textContent = 'Hey everyone is waiting on me!';
+}
+console.timeEnd('MyTimer')
+console.log('Next task')
